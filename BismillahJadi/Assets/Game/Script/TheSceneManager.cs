8f89d41bb52aa9d2c1f2b61265f1e26f760ef175
Kenzeroid <kenzeroid@gmail.com>
2018-03-09 09:56:54 +0700

    SceneMainMenu
    Ngatur folder, MainMenu, SceneManager

---
+++ BismillahJadi/Assets/Game/Script/TheSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheSceneManager : MonoBehaviour {
+
+	public void NextScene(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void ToWorldScene(){
+		SceneManager.LoadScene ("World");
+	}
+
+	public void PrevScene(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex - 1);
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+}
