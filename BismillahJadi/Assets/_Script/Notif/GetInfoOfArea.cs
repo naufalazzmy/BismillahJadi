22925e099908327b48e0962a82cf6f8675e2824c
Sugeng Prasetiyono <game.sugeng@gmail.com>
2018-03-10 19:48:51 +0700

    Notifkasi Progres 1
    KOtak putih = areanya..
    tekan i untuk menampilkan informasinya..
    UI kotak Dialog masih Belum..
    Lingkasran Belum ikut kamera

---
+++ BismillahJadi/Assets/_Script/Notif/GetInfoOfArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetInfoOfArea : MonoBehaviour {
+
+	// Use this for initialization
+	public static string getInfo(string name)
+    {
+        if (name == GameObject.Find("LVL1").name)
+        {
+            return "ini Level 1";
+        }
+        else if (name == GameObject.Find("LVL2").name)
+        {
+            return "INI level 2";
+        }
+        else if (name == GameObject.Find("LVL3").name)
+        {
+            return "ini level 3";
+        }
+        else
+            return null;
+    }
+
+}
---
+++ BismillahJadi/Assets/_Script/Notif/Notification.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notification : MonoBehaviour
+{
+    GameObject notif;
+    bool notifON = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        notif = GameObject.Find("Notif");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("GetInpo")&& notifON==true)
+        {
+            Debug.LogWarning(GetInfoOfArea.getInfo(transform.name));
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            notif.GetComponent<NotifController>().OnNotifBaloon();
+            notifON = true;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        {
+            Debug.LogError("Berhasil, " + transform.name);
+            notif.GetComponent<NotifController>().OffNotifBaloon();
+            notifON = false;
+        }
+    }
+}
